61b9105a01b7555779c552e80785a5f04d5d4f6d
Tymoteusz Wojcik <WojcikTymoteusz9@gmail.com>
2020-07-14 02:43:01 +0200

    adding new tasks

--- js/script.js
+++ js/script.js
@@ -10,12 +10,16 @@
     },
   ];
 
+
+
   const render = () => {
     let htmlString = "";
 
     for (task of tasks) {
       htmlString += `
-      <li>
+      <li 
+      ${task.done ? " style=\"text-decoration:line-through\" " : ""}
+      >
       ${task.content}
       </li>
       `;
@@ -23,8 +27,30 @@
     document.querySelector(".js-tasks").innerHTML = htmlString;
   }
 
+  const addNewTask = () => {
+    const newTaskContent = document.querySelector(".js-newTask").value.trim();
+
+    if (newTaskContent === "") {
+      return;
+    }
+
+    tasks.push({
+      content: newTaskContent,
+    });
+    render();
+  }
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+    addNewTask();
+  }
+
   const init = () => {
     render();
+
+    const form = document.querySelector(".js-form");
+
+    form.addEventListener("submit", onFormSubmit);
   }
   init();
 }
